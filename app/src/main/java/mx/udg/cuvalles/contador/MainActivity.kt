2a93628b82fa9222a0675b43ea492f1bd7459e31
Erick Guerrero <erickg@valles.udg.mx>
2020-09-30 16:53:01 -0500

    creado repositorio

---
+++ app/src/main/java/mx/udg/cuvalles/contador/MainActivity.kt
@@ -0,0 +1,21 @@
+package mx.udg.cuvalles.contador
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var contador = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btnIncrementar.setOnClickListener{
+            contador++
+            tvContador.text = "$contador"
+        }
+
+
+    }
+}
